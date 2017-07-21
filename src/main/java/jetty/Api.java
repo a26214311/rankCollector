261f80a76439676d904be8c7a9c8a101809cda21
shikong <a26214311@gmail.com>
2017-07-21 14:54:49 +0800

    f

--- src/main/java/jetty/Api.java
+++ src/main/java/jetty/Api.java
@@ -4,19 +4,40 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
+import lib.TimerTask;
+import senka.Calculator;
+import senka.Search;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = {"/api/*"}, loadOnStartup = 1)
 public class Api extends HttpServlet 
 {
 	
 	  static{
 		  System.out.print("star1111111111111111111111111111111111111t");
 	  }
+	  
 	  @Override 
-	  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		  System.out.println(123);
-		  response.getOutputStream().print("Hello World");
+	  public void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException{
+			String pathinfo = req.getPathInfo();
+			String path = pathinfo.substring(1);
+			String queryString = req.getQueryString();
+			Map<String, String[]> data = new HashMap<>();
+			if(queryString!=null){
+				data = ApiUtil.getParamsMap(queryString, "utf-8");
+			}
+			try {
+				String ret = handleData(path, data, req, response);
+				response.getOutputStream().print(ret);
+			} catch (Exception e) {
+				e.printStackTrace();
+				response.getOutputStream().print("error");
+			}
+
 	  }
 	  
 	  
@@ -24,5 +45,41 @@ public class Api extends HttpServlet
 	  @Override 
 	  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		  System.out.println(234);
+	  }
+		
+	  public static String handleData(String path,Map<String, String[]> data,final HttpServletRequest req, final HttpServletResponse resp)throws Exception{
+		String ret = "{\"r\":100}";
+		System.out.println(path);
+		System.out.println(data);
+		if(path.equals("test")){
+			ret = "{\"r\":110}";
+		}
+		if(path.equals("collect")){
+			senka.Collector.runCollector(data);
+			ret = "will run collector";
+		}
+		if(path.equals("rank")){
+			senka.Rank.runRank(data);
+			ret = "will run rank";
+		}
+		if(path.equals("seek")){
+			resp.setCharacterEncoding("utf-8");
+			resp.setContentType("text/plain");
+			ret = Search.seekByName(data);
+		}
+		if(path.equals("ranktask")){
+			TimerTask.rankTask();
+			ret = "will run rank task";
+		}
+		if(path.equals("forcecollect")){
+			TimerTask.collectorTask();
+			ret = "will run rank task";
+		}
+		if(path.equals("calrank")){
+			resp.setCharacterEncoding("utf-8");
+			resp.setHeader("Access-Control-Allow-Origin", "*");
+			ret = Calculator.calculator(data);
+		}
+		return ret;
 	  }
 }
---
+++ src/main/java/senka/Login.java
@@ -0,0 +1,106 @@
+package senka;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+
+
+
+public class Login {
+	public static void main(String[] args){
+		System.out.println("start");
+		try {
+			login("1127805853@qq.com","9876543210");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String login(String userid,String pwd){
+		try {
+		        String url = "http://ooi.moe/";
+		        String param = "login_id="+userid+"&password="+pwd+"&mode=1";
+		        String cookie = getOoiCookie(url,param);
+			String url2= "http://ooi.moe/kancolle";
+		        String s = HttpGet2(url2,param,cookie);
+			String str = "api_token=";
+			int n = s.indexOf(str);
+			if(n>0){
+				String s1 = s.substring(n+str.length());
+				int n2 = s1.indexOf("&");
+				String token = s1.substring(0,n2);
+				System.out.println(token);
+				return token;
+			}else{
+				return "";
+			}
+		} catch (Exception e) {
+			return "";
+		}
+
+	}
+	
+
+	
+
+	 
+	 public static String HttpGet2(String urlStr,String param,String cookie){
+		if(param == null || param.trim().length()<1){
+	            
+	        }else{
+	            urlStr +="?"+param;
+	        }   
+		try {
+			URL url = new URL(urlStr);
+	        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+	        conn.setConnectTimeout(10000);
+	        conn.setReadTimeout(12000);
+	        conn.setRequestProperty("Cookie", cookie);
+	        conn.setDoOutput(true);
+	        conn.setRequestMethod("GET");
+	        if(conn.getResponseCode() ==200){
+	        	BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+	            String line ;
+	            String result ="";
+	            while( (line =br.readLine()) != null ){
+	                result += line + "\n";
+	            }
+	            return result;
+	        }else{
+	            System.out.println("failed"+conn.getResponseCode()+conn.getResponseMessage());
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+	 
+	 
+	 
+	 public static String  getOoiCookie(String urlStr,String param) throws Exception{
+	         URL url = new URL(urlStr);
+	         HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+	         conn.setRequestMethod("POST");
+	         conn.setConnectTimeout(10000);
+	         conn.setReadTimeout(12000);
+	         conn.setInstanceFollowRedirects(false);
+	         conn.setDoInput(true);
+	         conn.setDoOutput(true);
+	         OutputStream os = conn.getOutputStream();     
+	         os.write(param.toString().getBytes("utf-8"));     
+	         os.close();         
+	         BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	         String line ;
+	         String result ="";
+	         while( (line =br.readLine()) != null ){
+	             result += line;
+	         }
+	         br.close();
+	         return conn.getHeaderField("Set-Cookie");
+	 }
+	 
+	    
+}
