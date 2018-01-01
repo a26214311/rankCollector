ee9326f548b807ef7a94cb7067c588d99054668e
a26214311 <a26214311@gmail.com>
2018-01-01 19:43:46 +0800

    Update Search.java

--- src/main/java/senka/Search.java
+++ src/main/java/senka/Search.java
@@ -94,7 +94,7 @@ public class Search {
 			int senkano = Integer.valueOf(senkadata.get("ts").toString());
 			if(isExpKeyTs(expts)){
 				int expno = Util.getRankDateNo(new Date(expts.getTime()+3600000*2));
-				if(expts.getMonth()<now.getMonth()){
+				if(expts.getFullYear()*12+expts.getMonth()<now.getFullYear()*12+now.getMonth()){
 					pointer1++;
 				}else if(expno==senkano){
 					front = new JSONObject();
